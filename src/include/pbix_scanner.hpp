b45eddc45083cc2b13c646d7bb24f71ff71040cd
Hugoberry <Hugoberry@users.noreply.github.com>
2024-03-17 23:24:23 +0000

    aadding data examples

--- src/include/pbix_scanner.hpp
+++ src/include/pbix_scanner.hpp
@@ -2,27 +2,29 @@
 
 #include "duckdb.hpp"
 
-namespace duckdb {
-class SQLiteDB;
+namespace duckdb
+{
+	class SQLiteDB;
 
-struct PbixBindData : public TableFunctionData {
-	string file_name;
-	string table_name;
+	struct PbixBindData : public TableFunctionData
+	{
+		string file_name;
+		string table_name;
 
-	vector<string> names;
-	vector<LogicalType> types;
+		vector<string> names;
+		vector<LogicalType> types;
 
-	idx_t max_rowid = 0;
-	bool all_varchar = false;
+		idx_t max_rowid = 0;
+		bool all_varchar = false;
 
-	idx_t rows_per_group = 122880;
-	SQLiteDB *global_db;
-};
+		idx_t rows_per_group = 122880;
+		SQLiteDB *global_db;
+	};
 
-class PbixScanFunction : public TableFunction {
-public:
-	PbixScanFunction();
-};
+	class PbixScanFunction : public TableFunction
+	{
+	public:
+		PbixScanFunction();
+	};
 
-
-} 
+}
