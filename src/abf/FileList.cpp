094b813a3fd8146bd064108cb936ba1f3c5e9030
Hugoberry <Hugoberry@users.noreply.github.com>
2024-03-22 11:53:27 +0000

    adding logBackupFiles

--- src/abf/FileList.cpp
+++ src/abf/FileList.cpp
@@ -7,9 +7,9 @@ FileList FileList::from_xml(tinyxml2::XMLElement& element) {
 
     XMLElement* backupFileElement = element.FirstChildElement("BackupFile");
     while (backupFileElement) {
-        BackupFile file = BackupFile::from_value(*backupFileElement,"UTF-8");
+        LogBackupFile file = LogBackupFile::from_value(*backupFileElement,"UTF-8");
         if (!fileList.BackupFiles) {
-            fileList.BackupFiles = std::vector<BackupFile>{};
+            fileList.BackupFiles = std::vector<long>{};
         }
         fileList.BackupFiles->push_back(file);
         backupFileElement = backupFileElement->NextSiblingElement("BackupFile");
--- src/abf/FileList.h
+++ src/abf/FileList.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <vector>
-#include "BackupFile.h"
+#include "LogBackupFile.h"
 #include "tinyxml2.h"
 
 class FileList {
 public:
-    std::optional<std::vector<BackupFile>> BackupFiles;
+    std::optional<std::vector<LogBackupFile>> BackupFiles;
 
     static FileList from_xml(tinyxml2::XMLElement& element);
 };
